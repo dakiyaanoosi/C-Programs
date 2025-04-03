56d4b3b796c2125996c0cb965fd4e6014e0ba2a4
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2025-04-03 11:30:21 +0530

    Create 4.c

---
+++ Assignment7/4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,j,index=-1;
+    char string[64], pattern[64];
+    printf("Enter the string : ");
+    scanf("%63s",string);
+    printf("Enter the pattern : ");
+    scanf("%63s",pattern);
+
+    int sl=strlen(string);
+    int pl=strlen(pattern);
+
+    for(i=0;i<=sl-pl;i++)
+    {
+        for(j=0;j<pl;j++)
+        {
+            if(string[i+j]!=pattern[j])
+            break;
+        }
+        if(j==pl)
+        {
+            index=i;
+            break;
+        }
+    }
+    printf("%d\n",index);
+    return 0;
+}

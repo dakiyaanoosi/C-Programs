3e55dd867c5cb51e8b44c5374ba48742906ec9d5
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2025-04-05 09:25:13 +0530

    Update 4.c

--- Assignment8/4.c
+++ Assignment8/4.c
@@ -16,30 +16,37 @@ long double permutation(int n,int r)
 }
 int main()
 {
+    int n,r,ch;
     printf("1.Find Factorial\n2.Combination\n3.Permutatioin\n\nEnter your choice [1/2/3] : ");
-    int ch;
     scanf("%d",&ch);
     switch(ch)
     {
         case 1 : printf("Enter the number : ");
-		 long long int n;
                  scanf("%d",&n);
+		 if(n<0)
+	         printf("Factorial is not defined for negative numbers!!\n");
+		 else
 		 printf("The Factrorial is : %llu\n",factorial(n));
 		 break;
-        case 2 : long r;
-		 printf("Enter 'n' : ");
+        case 2 : printf("Enter 'n' : ");
 		 scanf("%d",&n);
 		 printf("Enter 'r' : ");
 		 scanf("%d",&r);
+		 if(n<0 || r<0 || n<r)
+		 printf("Invalid input! Ensure that n>=r>=0.\n");
+		 else
 		 printf("%dC%d : %.0Lf\n",n,r,combination(n,r));
 		 break;
         case 3 : printf("Enter 'n' : ");
 		 scanf("%d",&n);
 		 printf("Enter 'r' : ");
 		 scanf("%d",&r);
+		 if(n<0 || r<0 || n<r)
+		 printf("Invalid input! Ensure that n>=r>=0.\n");
+		 else
 		 printf("%dP%d : %.0Lf\n",n,r,permutation(n,r));
 		 break;
-	default : printf("Invalid choice !!");
+	default : printf("Invalid choice !!\n");
     }
 }
     

af9d6b0ec14f7615e9ab5e7193e6efa61f54f522
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2025-04-25 21:04:55 +0530

    Add File

---
+++ Assignment9/2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+float sum(int num, double den, int terms)
+{
+    if(terms==0)
+    return 0;
+    return (num/den)+sum(num+2, den+2.2, terms-1);
+}
+int main()
+{
+    int terms,d;
+    printf("Number of terms : "); scanf("%d",&terms);
+    printf("Number of decimal places : "); scanf("%d",&d);
+    printf("Sum : %.*f\n",d,sum(2, 1.3, terms));
+    return 0;
+}

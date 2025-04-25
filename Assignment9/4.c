1d4500cd091a3b00a431c34658e3d56421650e04
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2025-04-25 21:21:03 +0530

    Add files

---
+++ Assignment9/4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void print(char text[100], int l)
+{
+    if(l<0)
+    {
+        printf("\n"); return;
+    }
+    printf("%c",text[l]);
+    print(text,l-1);
+}
+int main()
+{
+    char text[100];
+    printf("Enter a text : "); scanf("%99s",&text);
+    int l=strlen(text);
+    print(text,l-1);
+}
